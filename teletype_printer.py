8412961f28d0b8122eca302c4edbec1c1e2d00b7
David Xu <dxaviud@gmail.com>
2021-01-09 20:59:00 -0500

    refactor

--- teletype_printer.py
+++ teletype_printer.py
@@ -1,22 +1,23 @@
-#Teletype printer
-
 import time
 import random
 
-time_delay = 0.08
-string_to_print = "Hello World! This program is a teletype printer. It looks cool, but that's pretty much all it's good for. :D"
+time_delay = 0.05
+program_info = "This program simulates a teleprinter. It looks cool, but that's pretty much it."
 
 def teletype_print(what_to_print):
-    for ch in what_to_print:
-        print(ch, end = "")
-        if (ch == " "):
-            time.sleep(3*time_delay)
+    for character in what_to_print:
+        print(character, end = "")
+        if (character == " "):
+            time.sleep(time_delay)
         else:
-            time.sleep(time_delay + random.randint(0,10)/100.0)
+            time.sleep(time_delay + random.randint(0, 20)/100)
     print()
 
-teletype_print(string_to_print)
-
-user_input = input("Enter some text to teletype: ")
-
-teletype_print(user_input)
+while (True):
+    user_input = input("Enter some text to print ('i' for info, 'q' for quit): ")
+    if user_input == "q":
+        break
+    elif user_input == "i":
+        teletype_print(program_info)
+    else:
+        teletype_print(user_input)
