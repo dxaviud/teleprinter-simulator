4ca3f7953a19ae02605e4d2fe7db2230d54e9409
David Xu <dxaviud@gmail.com>
2021-02-18 22:21:45 -0500

    pep8 format

--- teletype_printer.py
+++ teletype_printer.py
@@ -4,17 +4,20 @@ import random
 time_delay = 0.04
 program_info = "This program simulates a teleprinter. It looks cool, but that's pretty much it."
 
+
 def teletype_print(what_to_print):
     for character in what_to_print:
-        print(character, end = "")
+        print(character, end="")
         if (character == " "):
             time.sleep(time_delay)
         else:
             time.sleep(time_delay + random.randint(0, 20)/100)
     print()
 
+
 while (True):
-    user_input = input("Enter some text to print ('i' for info, 'q' for quit): ")
+    user_input = input(
+        "Enter some text to print ('i' for info, 'q' for quit): ")
     if user_input == "q":
         break
     elif user_input == "i":
